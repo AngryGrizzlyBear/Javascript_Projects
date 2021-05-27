7283ad7f0ae2329339f2c808a00115b98bd61368
Evan Longwood <evan.longwood@pegs.com>
2021-05-27 15:14:54 -0700

    nesting examples

--- Controlling-Program-Logic-And-Flow/app.js
+++ Controlling-Program-Logic-And-Flow/app.js
@@ -20,3 +20,15 @@ else if (rating === 2) {
 } else {
     console.log("You need re-evaluation")
 }
+
+let password = 'kitty';
+
+if (password.length >= 6){
+    if(password.indexOf(' ') === -1){
+     console.log('Valid Password!')   
+    } else {
+        console.log('Password is long enough, but cannot contain spaces');
+    }
+} else {
+    console.log("Password must be longer")
+}
