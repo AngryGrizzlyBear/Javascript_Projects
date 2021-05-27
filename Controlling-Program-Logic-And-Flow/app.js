1d43c2e9f4e91837065f46b204ef70273655b159
Evan Longwood <evan.longwood@pegs.com>
2021-05-27 14:52:45 -0700

    Conditional Statements

--- Controlling-Program-Logic-And-Flow/app.js
+++ Controlling-Program-Logic-And-Flow/app.js
@@ -1,2 +1,20 @@
-alert("it's working!")
-console.log(3+4)
+if (1 === 1 ) {
+    console.log("It's True!")
+}
+
+
+// Example 2
+
+let rating = 3
+
+if (rating === 3 ) {
+    console.log('YOU ARE A SUPERSTAR!')
+
+}
+
+// Example 3
+let num = 37;
+
+if (num % 2 !== 0  ){
+    console.log("Odd Number")
+}
