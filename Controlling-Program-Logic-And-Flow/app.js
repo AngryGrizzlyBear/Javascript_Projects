544f7b406534aea455ff139b80baa2ef356cb546
Evan Longwood <evan.longwood@pegs.com>
2021-06-15 14:31:54 -0700

    challenge 3

--- Controlling-Program-Logic-And-Flow/app.js
+++ Controlling-Program-Logic-And-Flow/app.js
@@ -62,3 +62,12 @@ function average(arr){
     let res = total / arr.length;
     // return res;
 }
+function isPangram(sentence){
+    let lowerCased = sentence.toLowerCase();
+    for(let char of 'abcdefghijklmnopqrstuvwxyz'){
+       if(lowerCased.indexOf(char) === -1){
+           return false
+       }
+    }
+    return true;
+}
